9d7039d477d5ec630973802047a7a83cdde20171
Rayder <ch.l.rayder@gmail.com>
2022-07-18 20:47:12 +0500

    REST ВСЕ

--- ApiDRF/urls.py
+++ ApiDRF/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path, include
 from .views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = routers.SimpleRouter()
 router.register(r'product', ProductViewSet, basename='product')
@@ -9,5 +10,8 @@ router.register(r'product', ProductViewSet, basename='product')
 urlpatterns = [
     path('v1/drf-auth/', include('rest_framework.urls')), # Session-based authentication
     path('v1/', include(router.urls)),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     ]
